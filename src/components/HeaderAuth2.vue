7e942d13b5bec1e56db9571ae7d34723ebb8bcba
ユーザー名 <come.vanho330@icloud.com>
2021-03-08 16:08:39 +0900

    add new file

---
+++ src/components/HeaderAuth2.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="HeaderTodo">
+  <p>Just Do it.</p>
+  
+</div>
+</template>
+
+<style scoped>
+.HeaderTodo{
+  background: #fda634;
+  height: 70px;
+  width: 100%;
+  margin : 5px auto;
+  padding : 10px;
+  
+}
+
+.HeaderTodo p{
+  color: #f8f8f8;
+  font-size: 40px;
+  font-weight: bold;
+}
+
+@media screen and (max-width : 480px){
+  .HeaderTodo{
+    width: 100%;
+    float: none;
+    overflow: hidden;
+    margin-bottom: 0;
+    height: 10%;
+  }
+  .HeaderTodo p{
+     font-size: 100px;
+  }
+}
+
+@media screen and (max-width : 768px){
+  .HeaderTodo{
+    width: 100%;
+    float: none;
+    overflow: hidden;
+    margin-bottom: 0;
+    height: 10%;
+  }
+  .HeaderTodo p{
+     font-size: 100px;
+  }
+
+
+}
+
+</style>
